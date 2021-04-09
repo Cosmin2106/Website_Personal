daea9c422b8576a0485767835f42d0151eec5016
Cosmin Aprodu <cosmin.aprodu@tum.de>
2021-04-09 22:25:39 +0200

    Base design ready

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-bmd-drawer-x-size = 500px;
+var btnMenuOpen = false;
+
+$(document).ready(function() {
+  $('body').bootstrapMaterialDesign();
+  resizeCfg();
+});
+
+$(window).resize(resizeCfg);
+
+$('#menu-btn').on('click', function() {
+  if (window.innerWidth < 992) {
+    if (!btnMenuOpen) {
+      $('#front-panel').css('margin-left', 'var(--side-nav-dimen)');
+      btnMenuOpen = true;
+    } else {
+      $('#front-panel').css('margin-left', 0);
+      btnMenuOpen = false;
+    }
+  }
+});
+
+function resizeCfg() {
+  if (window.innerWidth >= 992) {
+    $('#front-panel').css('margin-left', 'var(--side-nav-dimen)');
+    btnMenuOpen = true;
+  } else {
+    $('#front-panel').css('margin-left', 0);
+    btnMenuOpen = false;
+  }
+}
